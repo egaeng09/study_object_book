3db8628b0b75d436d232dbf9a91b3b24cea5c5ce
egaeng09 <egaeng09@gmail.com>
2025-03-13 14:23:11 +0900

    feat(PercentDiscountPolicy): 비율 할인 정책 구현

---
+++ src/main/java/chapter2/PercentDiscountPolicy.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+import java.util.List;
+
+public class PercentDiscountPolicy extends DiscountPolicy {
+    private double percent;
+
+    public PercentDiscountPolicy(double percent, List<DiscountCondition> conditions) {
+        super(conditions);
+        this.percent = percent;
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return screening.getMovieFee().times(percent);
+    }
+}
