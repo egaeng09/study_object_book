8bfe91183c0105097c9d57726db19bf272f029b6
egaeng09 <egaeng09@gmail.com>
2025-03-25 18:10:17 +0900

    feat(Reservation): 접근자, 수정자 선언

--- src/main/java/chapter4/Reservation.java
+++ src/main/java/chapter4/Reservation.java
@@ -3,13 +3,45 @@ package chapter4;
 public class Reservation {
     private Customer customer;
     private Screening Screening;
-    private Money fee ;
+    private Money fee;
     private int audienceCount;
 
     public Reservation(Customer customer, Screening Screening, Money fee, int audienceCount) {
         this.customer = customer;
         this.Screening = Screening;
         this.fee = fee;
+        this.audienceCount = audienceCount;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Screening getScreening() {
+        return Screening;
+    }
+
+    public void setScreening(Screening screening) {
+        Screening = screening;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public int getAudienceCount() {
+        return audienceCount;
+    }
+
+    public void setAudienceCount(int audienceCount) {
         this.audienceCount = audienceCount;
     }
 }
