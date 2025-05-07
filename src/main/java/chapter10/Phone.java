4c43734bb0730e98b721e8b1e4f075fa56647cee
egaeng09 <egaeng09@gmail.com>
2025-05-07 17:29:03 +0900

    feat: Phone 클래스 추가

---
+++ src/main/java/chapter10/Phone.java
@@ -0,0 +1,44 @@
+package chapter10;
+
+import chapter4.Money;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Phone {
+    private Money amount;
+    private Duration seconds;
+    private List<Call> calls;
+
+    public Phone(Money amount, Duration seconds) {
+        this.amount = amount;
+        this.seconds = seconds;
+        this.calls = new ArrayList<>();
+    }
+
+    public void call(Call call) {
+        calls.add(call);
+    }
+
+    public List<Call> getCalls() {
+        return calls;
+    }
+
+    public Money getAmount() {
+        return amount;
+    }
+
+    public Duration getSeconds() {
+        return seconds;
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            result = result.plus(amount.times((double) call.getDuration().getSeconds() / seconds.getSeconds()));
+        }
+
+        return result;
+    }
+}
