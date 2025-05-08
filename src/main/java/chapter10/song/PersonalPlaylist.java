b912cf4b8c8a464224dc33d05f859977a1a23ed1
egaeng09 <egaeng09@gmail.com>
2025-05-08 11:22:59 +0900

    feat: 가수별 노래의 제목 관리 기능 추가

--- src/main/java/chapter10/song/PersonalPlaylist.java
+++ src/main/java/chapter10/song/PersonalPlaylist.java
@@ -3,5 +3,6 @@ package chapter10.song;
 public class PersonalPlaylist extends Playlist {
     public void remove(Song song) {
         getTracks().remove(song);
+        getSingers().remove(song.getSinger());
     }
 }
--- src/main/java/chapter10/song/Playlist.java
+++ src/main/java/chapter10/song/Playlist.java
@@ -1,20 +1,29 @@
 package chapter10.song;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Playlist {
     private List<Song> tracks;
+    private Map<String, String> singers;
 
     public Playlist() {
         this.tracks = new ArrayList<>();
+        this.singers = new HashMap<>();
     }
 
     public void append(Song song) {
         tracks.add(song);
+        singers.put(song.getSinger(), song.getTitle());
     }
 
     public List<Song> getTracks() {
         return new ArrayList<>(tracks);
+    }
+
+    public Map<String, String> getSingers() {
+        return new HashMap<>(singers);
     }
 }
