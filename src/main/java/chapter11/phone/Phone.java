8ba50e37c31d14dbf451c93140996ee0d0d5daf4
egaeng09 <egaeng09@gmail.com>
2025-05-20 14:56:28 +0900

    feat: 11장 기본 세팅

---
+++ src/main/java/chapter11/phone/Phone.java
@@ -0,0 +1,34 @@
+package chapter11.phone;
+
+import chapter4.Money;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Phone {
+    private List<Call> calls;
+
+    public Phone() {
+        this.calls = new ArrayList<>();
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            result = result.plus(calculateCallFee(call));
+        }
+
+        return result;
+    }
+
+    abstract protected Money calculateCallFee(Call call);
+
+
+    public void call(Call call) {
+        calls.add(call);
+    }
+
+    public List<Call> getCalls() {
+        return calls;
+    }
+}
---
+++ src/main/java/chapter11/phone/RegularPhone.java
@@ -0,0 +1,28 @@
+package chapter11.phone;
+
+import chapter4.Money;
+import java.time.Duration;
+
+public class RegularPhone extends Phone {
+
+    private Money amount;
+    private Duration seconds;
+
+    public RegularPhone(Money amount, Duration seconds) {
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    public Money getAmount() {
+        return amount;
+    }
+
+    public Duration getSeconds() {
+        return seconds;
+    }
+
+    @Override
+    protected Money calculateCallFee(Call call) {
+        return amount.times((double) call.getDuration().getSeconds() / seconds.getSeconds());
+    }
+}
