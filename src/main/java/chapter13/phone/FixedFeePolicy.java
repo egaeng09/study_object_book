cc85c5ac6924a34465c66271d296b7aaa846138f
egaeng09 <egaeng09@gmail.com>
2025-06-04 20:39:59 +0900

    refactor: 고정 요금 방식 구현
    - 기존 일반 요금제와 동일하기에 클래스 이름만 변경하였습니다.

--- src/main/java/chapter13/phone/FixedFeePolicy.java
+++ src/main/java/chapter13/phone/FixedFeePolicy.java
@@ -5,11 +5,11 @@ import chapter11.phone.composition.BasicRatePolicy;
 import chapter4.Money;
 import java.time.Duration;
 
-public class RegularPolicy extends BasicRatePolicy {
+public class FixedFeePolicy extends BasicRatePolicy {
     private Money amount;
     private Duration seconds;
 
-    public RegularPolicy(Money amount, Duration seconds) {
+    public FixedFeePolicy(Money amount, Duration seconds) {
         this.amount = amount;
         this.seconds = seconds;
     }
